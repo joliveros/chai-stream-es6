91c304ad02a83252f67dd479c5b8362c1bb51d04
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-04 03:02:05 -0500

    add: assert/expect for streams to index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-export function add(x, y) {
-  return x + y;
+const Promise = require('es6-promise');
+const highland = require('highland');
+
+function chaiStream(chai, utils) {
+  require('./assert')(chai, utils);
+  require('./expect')(chai, utils);
 }
+
+module.exports = chaiStream;
